e4423018b0b4a17a4590b32ac39803f0056dd039
Tiago Baldo <tgbaldo@gmail.com>
2017-11-03 21:44:39 -0200

    Helper que pega IP

---
+++ getClientIP.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* @author Tiago Baldo <tgbaldo@gmail.com>
+* @return $ipadress
+*/
+function getClientIP()
+{
+    if (getenv('HTTP_CLIENT_IP')) {
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    } else if (getenv('HTTP_X_FORWARDED_FOR')) {
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    } else if (getenv('HTTP_X_FORWARDED')) {
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    } else if(getenv('HTTP_FORWARDED_FOR')) {
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    } else if(getenv('HTTP_FORWARDED')) {
+       $ipaddress = getenv('HTTP_FORWARDED');
+    } else if(getenv('REMOTE_ADDR')) {
+        $ipaddress = getenv('REMOTE_ADDR');
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
